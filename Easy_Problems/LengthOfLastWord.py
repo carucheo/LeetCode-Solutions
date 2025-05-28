301f83e74d4e1903083d5a7cee2d8e39052b01fb
carucheo <eroblestena@gmail.com>
2025-05-28 04:27:32 -0700

    comment: added question hyperlink to comment block

--- Easy_Problems/LengthOfLastWord.py
+++ Easy_Problems/LengthOfLastWord.py
@@ -1,6 +1,6 @@
 """
 LeetCode Problem: 58. Length of Last Word
-Difficulty: Easy
+Link: https://leetcode.com/problems/length-of-last-word/
 
 Given a string s consisting of words and spaces, return the length of the last word in the string.
 A word is defined as a maximal substring consisting of non-space characters only.
--- Easy_Problems/Question28.cpp
+++ Easy_Problems/Question28.cpp
@@ -1,6 +1,6 @@
 /*
 LeetCode Problem: 28. Find the Index of the First Occurrence in a String
-Difficulty: Easy
+Link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
 Description:
 Given two strings needle and haystack, return the index of the first occurrence
